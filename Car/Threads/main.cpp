895752e0951aee5a09486d1e9e37fc4adc1ac5f1
ALFA <stassuperpro@gmail.com>
2024-04-24 16:10:08 +0300

    TEST

---
+++ Car/Threads/main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include <thread>
+using namespace std;
+using namespace std::chrono_literals;
+bool finish = false;
+void Plus() {
+	while (!finish)
+	{
+		cout << "+";
+		this_thread::sleep_for(1s);
+	}
+}
+void Minus() {
+	while (!finish)
+	{
+		cout << "-";
+		this_thread::sleep_for(1s);
+	}
+}
+void main()
+{
+    setlocale(LC_ALL, "");
+	//Plus();
+	//Minus();
+	thread plus_thr(Plus);
+	thread minus_thr(Minus);
+
+	cin.get();
+	finish = true;
+	if(minus_thr.joinable())minus_thr.join();
+	if (plus_thr.joinable())plus_thr.join();
+	if (plus_thr.joinable())plus_thr.join();
+	if (plus_thr.joinable())plus_thr.join();
+}
